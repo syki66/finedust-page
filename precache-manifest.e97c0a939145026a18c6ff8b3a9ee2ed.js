4b3053b981d94c7210bf7e3942ddecb40bbbf1df
syki66 <syeonki66@gmail.com>
2020-01-12 16:55:23 +0900

    Updates

---
+++ precache-manifest.e97c0a939145026a18c6ff8b3a9ee2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed429b9f4dcfbc371dc3e3a17fffc475",
+    "url": "/react_js_finedust_app/index.html"
+  },
+  {
+    "revision": "229a058ce837a59b7619",
+    "url": "/react_js_finedust_app/static/css/main.6735749a.chunk.css"
+  },
+  {
+    "revision": "1a58782b9ffb1d98a3e9",
+    "url": "/react_js_finedust_app/static/js/2.3bc903e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_js_finedust_app/static/js/2.3bc903e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "229a058ce837a59b7619",
+    "url": "/react_js_finedust_app/static/js/main.700d1012.chunk.js"
+  },
+  {
+    "revision": "761788548dbcc35b1216",
+    "url": "/react_js_finedust_app/static/js/runtime-main.5979e969.js"
+  }
+]);
